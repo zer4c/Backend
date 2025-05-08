b2ad557b224b9e6600bd548b0e962ce789e66ac6
Pablo Azurduy <pabloazurduy40@gmail.com>
2025-05-07 23:53:49 -0400

    refactor: add ":" after slash in product routes

--- src/routes/product_route.py
+++ src/routes/product_route.py
@@ -12,18 +12,18 @@ def add_product(product: Product):
 def all_products():
     return pc.get_products_response()
 
-@router.get("/{id}")
+@router.get("/:{id}")
 def get_product_id(id):
     return pc.get_products_id_response(id)
 
-@router.put("/{id}")
+@router.put("/:{id}")
 def update_product(product: Product, id):
     return pc.update_product_response(product, id)
 
-@router.patch("/{id}")
+@router.patch("/:{id}")
 def patch_product(product: Product, id):
     return pc.patch_product_response(product, id)
 
-@router.delete("/{id}")
+@router.delete("/:{id}")
 def remove_product(id):
     return pc.remove_product_response(id)
